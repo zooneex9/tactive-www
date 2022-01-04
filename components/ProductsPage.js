75888e01cb8b9f03289bd7e7040d767daf4449d1
Sonny Gzz <sonny@lagom.agency>
2022-01-03 21:22:05 -0600

    Update: added test phrase

--- components/ProductsPage.js
+++ components/ProductsPage.js
@@ -7,6 +7,7 @@ function ProductsPage({ products }) {
     <div className="container mx-auto px-6">
       <h3 className="text-gray-700 text-2xl font-medium">Juices</h3>
       <span className="mt-3 text-sm text-gray-500">El vic se la come.</span>
+      <span className="mt-3 text-sm text-gray-500">En efecto.</span>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
         {products.map((product) => (
           <ProductCard key={product._id} {...product} />
